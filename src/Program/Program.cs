4f9f55973c1bd85b2b7063fcf0f0ec4f4f29c6c2
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2022-05-19 19:20:15 -0300

    Ej hecho

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -22,8 +22,8 @@ namespace Full_GRASP_And_SOLID
 
             Recipe recipe = new Recipe();
             recipe.FinalProduct = GetProduct("Café con leche");
-            recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
-            recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
+            recipe.AddStep(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120);
+            recipe.AddStep(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60);
 
             IPrinter printer;
             printer = new ConsolePrinter();
@@ -73,5 +73,7 @@ namespace Full_GRASP_And_SOLID
             var query = from Equipment equipment in equipmentCatalog where equipment.Description == description select equipment;
             return query.FirstOrDefault();
         }
+
+        
     }
 }
